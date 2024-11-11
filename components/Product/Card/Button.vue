37bdde1332aa48b32e3da150b542522c8ec039fa
Otis <variety928@gmail.com>
2024-11-11 16:40:54 +0800

    first commit

---
+++ components/Product/Card/Button.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  buttonText: String,
+});
+</script>
+
+<template>
+  <button class="product-button">{{ buttonText }}</button>
+</template>
+
+<style scoped>
+.product-button {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  background-color: #007BFF;
+  color: white;
+  border: none;
+  border-radius: 0 0 8px 8px;
+  cursor: pointer;
+  text-align: center;
+  font-size: 16px;
+}
+</style>
